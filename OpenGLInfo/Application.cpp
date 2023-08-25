f27521b9c278560bcb48c7262fde94ea10c47ede
Shiravand <shiravandm96@gmail.com>
2023-08-25 21:03:09 +0330

    Adding more examples

---
+++ OpenGLInfo/Application.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+int main()
+{
+
+	// GLFW Initializations
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// Create Window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Loading OpenGl methods
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Set size of rendering window
+	glViewport(0, 0, 800, 600);
+
+
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
+
+
+	glfwTerminate();
+	return 0;
+}
--- READMe.md
+++ READMe.md
@@ -1,3 +1,11 @@
 # HelloGL
 
 Coding with learnopengl.com tutorial
+[OpenGL Info](OpenGLInfo)
+[Hello triangle](HelloTriangle)
+[Hello rectangle](HelloRectangle)
+[Two triangles using more vertices](TwoTrianglesWithMoreVertices)
+[Two triangles using 2 different VAOs and VBOs](TwoTrainglesWithDifferentVAOsAndVBOs)
+[Two triangles with different colors using another shader program](TwoTrainglesWithDifferentVAOsAndVBOs)
+[Using uniform to gradually change color of objects over time](GradualColorChange)
+[Fragment interpolation](FragmentInterpolation)
